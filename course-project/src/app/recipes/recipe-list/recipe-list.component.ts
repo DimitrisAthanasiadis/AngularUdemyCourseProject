66d4c0aeff13dfd7d675b0321106fdcf3ef32490
Dimitris Athanasiadis <dathanasiadis@net.goanalysis.gr>
2021-08-16 11:13:10 +0300

    Output recipe list components

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Pizza', 'Delicious pizza', 'https://www.simplyrecipes.com/thmb/YSlSLYrnOBfkzE3rD_uMSnA8dlA=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-LEAD-3-8aa37af554cf4445a1e3e54fdc66e974.jpg'),
+    new Recipe("Grilled brisket", "Delicious brisket", "https://assets.bonappetit.com/photos/57ad3f9cf1c801a1038bcadf/1:1/w_1920,c_limit/grilled-sliced-brisket.jpg")
+  ];
 
   constructor() { }
 
