fd31ac0abf6ece3e1870de3ecbdf44456e37711e
Dimitris Athanasiadis <dathanasiadis@net.goanalysis.gr>
2021-11-19 14:19:04 +0200

    Pass recipe data with event and property binding

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from "../recipe.model";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Pizza', 'Delicious pizza', 'https://www.simplyrecipes.com/thmb/YSlSLYrnOBfkzE3rD_uMSnA8dlA=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-LEAD-3-8aa37af554cf4445a1e3e54fdc66e974.jpg'),
     new Recipe("Grilled brisket", "Delicious brisket", "https://assets.bonappetit.com/photos/57ad3f9cf1c801a1038bcadf/1:1/w_1920,c_limit/grilled-sliced-brisket.jpg")
@@ -15,6 +16,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
